8fa6e25eb9a34d3bc0e308b4390fc275d394fa03
54NK37 <sanketfaraned2@gmail.com>
2024-06-27 16:40:45 +0530

    Expiration initial setup

---
+++ expiration/src/index.ts
@@ -0,0 +1,26 @@
+import { natsWrapper } from './nats/nats-wrapper';
+
+const start = async () => {
+    try {
+        if (!process.env.NATS_CLUSTER_ID || !process.env.NATS_URL || !process.env.CLIENT_ID || !process.env.REDIS_HOST) {// here we have to ckeck all env files are loaded properly
+            console.log(process.env.JWT_KEY, process.env.MONGO_URI, process.env.NATS_CLUSTER_ID, process.env.NATS_URL, process.env.CLIENT_ID)
+            throw new Error('Environment variables not defined')
+        }
+
+        await natsWrapper.connect(process.env.NATS_CLUSTER_ID!, process.env.CLIENT_ID!, process.env.NATS_URL!)
+
+        // handling closing at here because process.exit should be avoided in singleton class 
+        // refer nats-test for more details
+        natsWrapper.client.on('close', () => {
+            console.log('NATS connection closed')
+            process.exit()
+        })
+        process.on('SIGINT', () => natsWrapper.client.close())
+        process.on('SIGTERM', () => natsWrapper.client.close())
+        console.log('Started Expiration service!!!')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
